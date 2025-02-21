94c911dedd1a99b5de7fca365f2ffc01e692ad51
Tejas Mandre <tejas.mandre@nutanix.com>
2025-02-21 21:22:32 +0530

    improve message display logic and enhance UI layout

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use core::str;
 use std::{
-    io::{Read, Write}, net::TcpStream, process::exit, sync::Arc
+    io::{Read, Write},
+    net::TcpStream,
+    process::exit,
+    sync::Arc,
 };
 
 use iced::{
-    futures::{SinkExt, Stream, StreamExt},
-    stream,
-    widget::{column, text, text_input, Column},
-    Element, Subscription,
+    font::Weight, futures::{SinkExt, Stream, StreamExt}, stream, widget::{button, column, container, scrollable, text, text_input, Column, Row}, Color, Element, Font, Length, Subscription, Theme
 };
 
 use std::sync::{LazyLock, Mutex};
@@ -20,17 +20,17 @@ struct AppState {
     room_id: String,
     messages: Arc<Mutex<Vec<String>>>,
     current_message: String,
-    room_joined: bool
+    room_joined: bool,
 }
 
 impl Default for AppState {
     fn default() -> Self {
         AppState {
-            name: String::from("Tejas"),
+            name: String::new(),
             room_id: String::new(),
             messages: Arc::new(Mutex::new(Vec::new())),
             current_message: String::new(),
-            room_joined: false
+            room_joined: false,
         }
     }
 }
@@ -60,7 +60,6 @@ fn recv_updates() -> impl Stream<Item = Message> {
             let message = rx.select_next_some().await;
             op.send(Message::ReceivedMessage(message)).await.unwrap();
         }
-
     })
 }
 
@@ -95,7 +94,11 @@ fn update(app_state: &mut AppState, message: Message) {
             let s = s.trim().to_string();
             tcp_stream.write_all(s.as_bytes()).unwrap();
             {
-                app_state.messages.lock().unwrap().push(format!("You > {}", s));
+                app_state
+                    .messages
+                    .lock()
+                    .unwrap()
+                    .push(format!("You > {}", s));
             }
             app_state.current_message.clear();
         }
@@ -106,29 +109,66 @@ fn update(app_state: &mut AppState, message: Message) {
 }
 
 fn view(app_state: &AppState) -> Element<Message> {
-    let mut message_text_vec: Vec<Element<Message>> = Vec::new();
     let messages = app_state.messages.lock().unwrap();
 
-    for message in messages.iter() {
-        message_text_vec.push(text(message.clone()).into());
-    }
+    let messages_text_vec = messages
+        .iter()
+        .map(|msg| {
+            if msg.starts_with("You > ") {
+                text(msg.clone()).size(15).color(Color::BLACK).into()
+            } else {
+                text(msg.clone())
+                    .color(Color::from_rgb(255.0, 0.0, 0.0))
+                    .size(15)
+                    .into()
+            }
+        })
+        .collect::<Vec<Element<Message>>>();
 
     drop(messages);
 
-    let messages_column: Element<Message> = Column::from_vec(message_text_vec).into();
-
-    let message_input: Element<Message> = text_input("Type your message", &app_state.current_message)
-        .on_input(Message::CurrentMessageChanged)
-        .on_submit(Message::SendMessage(app_state.current_message.clone()))
+    let messages_column: Element<Message> = Column::from_vec(messages_text_vec).into();
+    let scrollable_messages: Element<Message> = scrollable(messages_column)
+        .height(Length::Fill)
+        .width(Length::Fill)
         .into();
 
-    column![messages_column, message_input].into()
+    let message_input: Element<Message> =
+        text_input("Type your message", &app_state.current_message)
+            .on_input(Message::CurrentMessageChanged)
+            .on_submit(Message::SendMessage(app_state.current_message.clone()))
+            .padding(10)
+            .size(16)
+            .into();
+
+    let send_button: Element<Message> = button("Send")
+        .on_press(Message::SendMessage(app_state.current_message.clone()))
+        .padding(10)
+        .into();
+
+    let input_row: Element<Message> = Row::new()
+        .push(message_input)
+        .push(send_button)
+        .spacing(10)
+        .height(Length::Shrink)
+        .into();
+
+    column![
+        scrollable_messages,
+        container(input_row).padding(10).width(Length::Fill)
+    ]
+    .spacing(10)
+    .height(Length::Fill)
+    .padding(10)
+    .into()
+
 }
 
 #[tokio::main]
 async fn main() {
-    let _ = iced::application("MyApp", update, view)
+    let _ = iced::application("Lets Chat", update, view)
         .subscription(subscription)
+        .theme(|app_state: &AppState| Theme::GruvboxLight)
         .run()
         .unwrap();
     exit(0);
